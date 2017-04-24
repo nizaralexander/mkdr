3cffbb9d3a350b669dc7e19c1b7967668fc8908c
nizaralexander <ubootlop@gmail.com>
2017-04-24 17:08:42 +0700

    here

--- app/src/main/java/com/mycompany/myapp/MainActivity.java
+++ app/src/main/java/com/mycompany/myapp/MainActivity.java
@@ -2,14 +2,7 @@ package com.mycompany.myapp;
 
 import android.app.*;
 import android.os.*;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-import java.io.*;
-import javax.xml.ws.*;
-import javax.xml.ws.http.*;
-import javax.xml.transform.*;
-import javax.xml.transform.stream.*;
+
 public class MainActivity extends Activity 
 {
     @Override
